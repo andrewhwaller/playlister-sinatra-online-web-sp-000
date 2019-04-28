bb6fe5e2fb78341c92973061de8a0a839706babb
Andrew Waller <andrewhwaller@gmail.com>
2019-04-28 21:53:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,8 @@
-class SongsController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
+class ArtistsController < ApplicationController
 
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
 
-get '/songs' do
-  @songs = Song.all
-  erb :'/songs/index'
 end
