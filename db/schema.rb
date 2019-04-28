3bcf079b6410b423d510c0f9e050e7c4206050e3
Andrew Waller <andrewhwaller@gmail.com>
2019-04-28 03:41:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_033750) do
+ActiveRecord::Schema.define(version: 2019_04_28_034127) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
     t.string "name"
   end
 
