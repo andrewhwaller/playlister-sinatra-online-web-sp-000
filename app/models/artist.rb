615cedf8f0d1a2435e1b31f01eb1149af9d74029
Andrew Waller <andrewhwaller@gmail.com>
2019-04-28 03:46:51 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,8 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, :through => :songs
 
+  def slugify
+    slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    slug
+  end
 end
