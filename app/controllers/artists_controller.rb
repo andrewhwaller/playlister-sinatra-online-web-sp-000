28b2d27bc38a13aa107bd973320fb11b54fc9d0f
Andrew Waller <andrewhwaller@gmail.com>
2019-04-28 21:55:14 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,4 +4,8 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
     erb :'/artists/index'
   end
+
+  get '/artists/:slug' do
+    #show page with links to song artist and genres
+  end
 end
